fce23e66516e0caaef0542c6499c643cd54a6de7
Lmotorness <jhw0222@iCloud.com>
2026-09-04 15:26:58 +0900

    움직임 테스트용

--- Assets/Player/Scripts/TorsoMover.cs
+++ Assets/Player/Scripts/TorsoMover.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class TorsoMover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveForce = 15f;
+    public float jumpForce = 8f;
 
-    // Update is called once per frame
+    Rigidbody2D rb;
+    bool grounded;
+
+    void Awake() { rb = GetComponent<Rigidbody2D>(); }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
+
+    void FixedUpdate()
+    {
+        float dir = 0f;
+        if (Input.GetKey(KeyCode.A)) dir -= 1f;
+        if (Input.GetKey(KeyCode.D)) dir += 1f;
+        rb.AddForce(Vector2.right * dir * moveForce);
+    }
+
+    void OnCollisionEnter2D(Collision2D c) { if (c.collider.CompareTag("Ground")) grounded = true; }
+    void OnCollisionExit2D(Collision2D c) { if (c.collider.CompareTag("Ground")) grounded = false; }
 }
